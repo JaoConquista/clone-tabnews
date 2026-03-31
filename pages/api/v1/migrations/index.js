42a2b13f130e8443299c742fb11e03db86876043
JaoConquista <conquistajoaovictor@gmail.com>
2026-03-31 01:22:03 -0300

    (draft): endpoint migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,6 +4,11 @@ import migrationRunner from "node-pg-migrate";
 export default async function migrations(request, response) {
   const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
+    migrationsTable: "pgmigrations",
+    dir: "infra/migrations",
+    direction: "up",
+    verbose: true,
+    dryRun: true,
   });
 
   return response.status(200).json(migrations);
